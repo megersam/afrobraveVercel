26fce0c846f9e479cbf3071d5e64323336d1ce92
megersam <megersabmuleta01@gmail.com>
2023-10-25 15:01:12 +0300

    frontend code url base called from the vercel url

--- backend/controller/shop.js
+++ backend/controller/shop.js
@@ -47,7 +47,7 @@ router.post("/create-shop", upload.single("file"), async (req, res, next) => {
 
     const activationToken = createActivationToken(seller);
 // the activation link
-    const activationUrl = `http://localhost:3000/seller/activation/${activationToken}`;
+    const activationUrl = `https://afrobrave.vercel.app/seller/activation/${activationToken}`;
 
     try {
       await sendMail({
--- backend/controller/user.js
+++ backend/controller/user.js
@@ -51,7 +51,7 @@ router.post("/create-user", upload.single("file"), async (req, res, next) => {
 
     const activationToken = createActivationToken(user);
 
-    const activationUrl = `http://localhost:3000/activation/${activationToken}`;
+    const activationUrl = `https://afrobrave.vercel.app/activation/${activationToken}`;
 
     try {
       await sendMail({
