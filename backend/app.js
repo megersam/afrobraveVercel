bd2d083065862096824f0586617295b738c83260
megersam <megersabmuleta01@gmail.com>
2023-10-25 17:49:20 +0300

    none

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,7 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 // app.use("/", express.static(path.join(__dirname,"./uploads")));
-app.use("/test", (req, res) => {
+app.use("/", (req, res) => {
   res.send("Backend Working!");
 });
 
