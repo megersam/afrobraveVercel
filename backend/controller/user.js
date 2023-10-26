6a9a69bfbb6c6ac97552a2a242e3d4d282ae1e2f
megersam <megersabmuleta01@gmail.com>
2023-10-26 10:38:37 +0300

    CORS notworking and try again

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -48,7 +48,7 @@ router.post("/create-user", async (req, res, next) => {
 
     const activationToken = createActivationToken(user);
 
-    const activationUrl = `https://eshop-tutorial-pyri.vercel.app/activation/${activationToken}`;
+    const activationUrl = `https://afrobrave.vercel.app/activation/${activationToken}`;
 
     try {
       await sendMail({
