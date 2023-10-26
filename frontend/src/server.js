1e95761ce3b892411ecb855db3ed474d3c939026
megersam <megersabmuleta01@gmail.com>
2023-10-26 11:24:12 +0300

    bakend url

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,3 @@
 export const server = "https://afrobrave-backend.vercel.app/api/v2";
 
-// export const backend_url = "https://afrobrave-backend.vercel.app/";
+export const backend_url = "https://afrobrave-backend.vercel.app/";
